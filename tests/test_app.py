2f1268be6a81988e21c0347a990e0940736b2022
James Phillips <terrace.bowling-0u@icloud.com>
2024-09-13 10:10:18 +0100

    Fix Bandit warnings and Flake8 linting errors

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,12 @@
 from app.app import app
 
+
 def test_hello():
     response = app.test_client().get('/')
     # nosec B101
     assert response.status_code == 200
     assert response.data == b'Hello, World!'
+
 
 def test_name():
     response = app.test_client().get('/name/John')
@@ -12,11 +14,13 @@ def test_name():
     assert response.status_code == 200
     assert response.data == b'Hello, John!'
 
+
 def test_age():
     response = app.test_client().get('/age/25')
     # nosec B101
     assert response.status_code == 200
     assert response.data == b'You are 25 years old.'
+
 
 def test_404():
     response = app.test_client().get('/nonexistent')
